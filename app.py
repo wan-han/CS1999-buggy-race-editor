f871147a29b07046fe0cc33e485a273479c569ea
han <77970646+wan-han@users.noreply.github.com>
2022-06-15 13:03:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import sqlite3 as sql
+import urllib.request
 
 # app - The flask application where all the magical things are configured.
 app = Flask(__name__)
@@ -17,52 +18,106 @@ BUGGY_RACE_SERVER_URL = "https://rhul.buggyrace.net"
 def home():
     return render_template('index.html', server_url=BUGGY_RACE_SERVER_URL)
 
+def tooLong():
+    con = sql.connect(DATABASE_FILE)
+    con.row_factory = sql.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM buggies")
+    return cur.fetchone();
+
 #------------------------------------------------------------
 # creating a new buggy:
 #  if it's a POST request process the submitted data
 #  but if it's a GET request, just show the form
 #------------------------------------------------------------
+
 @app.route('/new', methods = ['POST', 'GET'])
 def create_buggy():
     if request.method == 'GET':
-        return render_template("buggy-form.html")
+        record = tooLong()
+        return render_template("buggy-form.html", buggy = record)
     elif request.method == 'POST':
         msg=""
         qty_wheels = request.form['qty_wheels']
+        flag_color = request.form['flag_color']
+        flag_color_secondary = request.form['flag_color_secondary']
+        flag_pattern = request.form['flag_pattern']
+
+        if not qty_wheels.isdigit():
+            msg = f"Entered item is not a numerical value {qty_wheels}"
+            return render_template("updated.html", msg = msg)
+
+        if flag_color == flag_color_secondary:
+            msg = f"Primary and secondary colour cannot be the same. "
+            return render_template("updated.html", msg = msg)
+
         try:
             with sql.connect(DATABASE_FILE) as con:
                 cur = con.cursor()
                 cur.execute(
-                    "UPDATE buggies set qty_wheels=? WHERE id=?",
-                    (qty_wheels, DEFAULT_BUGGY_ID)
+                    "UPDATE buggies set qty_wheels=?, flag_color=?, flag_color_secondary=?, flag_pattern=? WHERE id=?",
+                    (qty_wheels, flag_color, flag_color_secondary, flag_pattern, DEFAULT_BUGGY_ID)
                 )
                 con.commit()
                 msg = "Record successfully saved"
+
         except:
             con.rollback()
             msg = "error in update operation"
+
         finally:
             con.close()
+            
         return render_template("updated.html", msg = msg)
 
 #------------------------------------------------------------
 # a page for displaying the buggy
 #------------------------------------------------------------
+@app.route('/delete/<buggy_id>')
+def delete_buggy(buggy_id):
+    try:
+        with sql.connect(DATABASE_FILE) as con:
+            cur= con.cursor()
+            cur.execute(
+                "DELETE FROM buggies WHERE id=?", (buggy_id,)
+            )
+            con.commit()
+            msg = "Record successfully saved"
+    except:
+        con.rollback()
+        msg = "error in update operation"
+    finally:
+        con.close()
+        return render_template("updated.html", msg = msg)
+        
 @app.route('/buggy')
 def show_buggies():
-    con = sql.connect(DATABASE_FILE)
-    con.row_factory = sql.Row
-    cur = con.cursor()
-    cur.execute("SELECT * FROM buggies")
-    record = cur.fetchone(); 
+    record = tooLong()
     return render_template("buggy.html", buggy = record)
 
 #------------------------------------------------------------
 # a placeholder page for editing the buggy: you'll need
 # to change this when you tackle task 2-EDIT
 #------------------------------------------------------------
+
 @app.route('/edit')
 def edit_buggy():
+    return render_template("buggy-form.html")
+
+@app.route('/poster')
+def poster():
+    return render_template("poster.html")
+
+@app.route('/info')
+def info():
+    req = urllib.request.Request('https://rhul.buggyrace.net/specs/data/types.json')
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        # print(the_page)
+    return render_template("info.html")
+
+@app.route('/buggy-form')
+def buggyform_show():
     return render_template("buggy-form.html")
 
 #------------------------------------------------------------
@@ -74,13 +129,13 @@ def edit_buggy():
 #  it. There's no .html template here because it's *only* returning
 #  the data, so in effect jsonify() is rendering the data.
 #------------------------------------------------------------
+
 @app.route('/json')
 def summary():
     con = sql.connect(DATABASE_FILE)
     con.row_factory = sql.Row
     cur = con.cursor()
     cur.execute("SELECT * FROM buggies WHERE id=? LIMIT 1", (DEFAULT_BUGGY_ID))
-
     buggies = dict(zip([column[0] for column in cur.description], cur.fetchone())).items() 
     return jsonify({ key: val for key, val in buggies if (val != "" and val is not None) })
 
